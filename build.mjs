5a856de84efeaafe888515b1bc9b845de9bf995a
Giancarlo Mariot <giancarlo.mariot@sky.uk>
2025-12-05 12:02:07 +0000

    Splitting massive index file

--- build.mjs
+++ build.mjs
@@ -63,67 +63,201 @@ function renderTableRows(list, ignoreIndex = false) {
   return rows.join('\n');
 }
 
-let numberSection = 0;
-let numberTopic = 0;
+// let numberSection = 0;
+// let numberTopic = 0;
 
-function nextSection() {
-  numberSection++;
-  numberTopic = 1;
-  return `${numberSection}.`;
-}
+// function nextSection() {
+//   numberSection++;
+//   numberTopic = 1;
+//   return `${numberSection}.`;
+// }
 
-function nextTopic() {
-  const lastUsed = `${numberSection}.${numberTopic}`;
-  numberTopic++;
-  return lastUsed;
-}
+// function nextTopic() {
+//   const lastUsed = `${numberSection}.${numberTopic}`;
+//   numberTopic++;
+//   return lastUsed;
+// }
 
-function formatTopic(source) {
-  const headingNumber = nextTopic();
-  return source.replace('%section%', headingNumber);
-}
+// function formatTopic(source) {
+//   const headingNumber = nextTopic();
+//   return source.replace('%section%', headingNumber);
+// }
 
-const template = fs.readFileSync('index-template.html', 'utf8');
+// const template = fs.readFileSync('index-template.html', 'utf8');
 
 const tableSwadesh = renderTableRows(swadesh);
 const tableSilm100 = renderTableRows(silm100, true);
 
-const fileNames = [
-  { file: 'intro', pass: true },
-  { section: 'Fundamentals' },
-  { file: 'pronouns' },
-  { file: 'copula' },
-  { file: 'nouns' },
+const skeleton = [
+  {
+    file: 'intro',
+  },
+  {
+    name: 'Fundamentals',
+    anchor: 'fundamentals',
+    sections: [
+      {
+        name: 'Pronouns',
+        anchor: 'pronouns',
+        file: 'pronouns'
+      },
+      {
+        name: 'Copular system',
+        anchor: 'copular',
+        file: 'copula'
+      },
+      {
+        name: 'Basic nouns',
+        anchor: 'basic_nouns',
+        file: 'nouns'
+      },
+      {
+        name: 'Basic verbs',
+        anchor: 'basic_verbs',
+        file: 'verbs1'
+      },
+      {
+        name: 'Consonant mutations',
+        anchor: 'mutations',
+        file: 'mutations'
+      },
+    ]
+  },
+  {
+    name: 'Articles, Adjectives, and Basic Syntax',
+    anchor: 'basic',
+    sections: [
+      {
+        name: 'Articles',
+        anchor: 'articles',
+        file: 'articles'
+      },
+      {
+        name: 'Adjectives',
+        anchor: 'adjectives',
+        file: 'adjectives'
+      },
+      {
+        name: 'Numerals',
+        anchor: 'numerals',
+        file: 'numerals'
+      },
+      {
+        name: 'Sentence Structure',
+        anchor: 'sentence_structure',
+        file: 'sentence'
+      },
+      {
+        name: 'Adverbs',
+        anchor: 'adverbs',
+        file: 'adverbs'
+      },
+      {
+        name: 'Negation',
+        anchor: 'negation',
+        file: 'negation'
+      },
+      {
+        name: 'Yes and No',
+        anchor: 'yes_no',
+        file: 'yesno'
+      },
+      {
+        name: 'Conjunctions',
+        anchor: 'conjunctions',
+        file: 'conjunctions'
+      },
+    ]
+  }
 ];
 
-let finalHtml = template;
+let finalHtml = fs.readFileSync('index-template.html', 'utf8');
+let menu = '<!--MENU-->';
 
-fileNames.forEach((fileObj) => {
-  const { section, file: fileName, pass } = fileObj;
-  let formatted;
+let i = 0;
+skeleton.forEach((item) => {
+  const { file, name, anchor, sections } = item;
 
-  if (section) {
-    const currentSectionNumber = nextSection();
-    formatted = `
+  console.log('- Processing', file, name, anchor, sections?.length);
+
+  if (!!file && !!name) {
+    throw `Can't have file and name at the same time! ${file} - ${name}`;
+  }
+
+  if (file) {
+    const sectionHtml = fs.readFileSync(`./src/${file}.html`, 'utf8');
+    finalHtml = finalHtml.replace(`<!--PLACEHOLDER-->`, sectionHtml);
+  } else
+
+  if (name) {
+    i++;
+    const formatted = `
 <h1>
-  ${currentSectionNumber} ${section}
+  ${i}. ${name}
 </h1>
 <!--PLACEHOLDER-->
   `
-  } else {
-    const sectionHtml = fs.readFileSync(`./src/${fileName}.html`, 'utf8');
+    finalHtml = finalHtml.replace(`<!--PLACEHOLDER-->`, formatted);
 
-    if (!pass) {
-      formatted = formatTopic(sectionHtml);
-    } else {
-      formatted = sectionHtml;
-    }
+    menu = menu.replace('<!--MENU-->', `    <li>\n      <a href="#${anchor}">${i}. ${name}</a>\n      <ul>\n<!--MENU-->\n      </ul>\n    </li>`);
   }
 
-  finalHtml = finalHtml.replace(`<!--PLACEHOLDER-->`, formatted);
+  if (sections) {
+    let j = 0;
+    sections.forEach((section) => {
+      console.log('- - Processing', section.file, section.anchor, section.name);
+      j++;
+      let sectionHtml = fs.readFileSync(`./src/${section.file}.html`, 'utf8');
+      sectionHtml = sectionHtml.replace('%section%', `${i}.${j}`);
+      finalHtml = finalHtml.replace(`<!--PLACEHOLDER-->`, sectionHtml);
+      menu = menu.replace('<!--MENU-->', `        <li><a href="#${section.anchor}">${i}.${j} ${section.name}</a></li>\n<!--MENU-->`);
+    });
+    menu = menu.replace('\n<!--MENU-->', '');
+    menu = `${menu}\n<!--MENU-->`;
+  }
 });
 
+menu = menu.replace('\n<!--MENU-->', '');
+
+
+
+// const fileNames = [
+//   { file: 'intro', pass: true },
+//   { section: 'Fundamentals' },
+//   { file: 'pronouns' },
+//   { file: 'copula' },
+//   { file: 'nouns' },
+// ];
+
+// let finalHtml = template;
+
+// fileNames.forEach((fileObj) => {
+//   const { section, file: fileName, pass } = fileObj;
+//   let formatted;
+
+//   if (section) {
+//     const currentSectionNumber = nextSection();
+//     formatted = `
+// <h1>
+//   ${currentSectionNumber} ${section}
+// </h1>
+// <!--PLACEHOLDER-->
+//   `
+//   } else {
+//     const sectionHtml = fs.readFileSync(`./src/${fileName}.html`, 'utf8');
+
+//     if (!pass) {
+//       formatted = formatTopic(sectionHtml);
+//     } else {
+//       formatted = sectionHtml;
+//     }
+//   }
+
+//   finalHtml = finalHtml.replace(`<!--PLACEHOLDER-->`, formatted);
+// });
+
 finalHtml = finalHtml
+  .replace('<!--MENU-->', menu)
   .replace('<!--TABLE_SWADESH-->', tableSwadesh)
   .replace('<!--TABLE_SILM100-->', tableSilm100);
 
